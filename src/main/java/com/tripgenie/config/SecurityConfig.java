37bb95aa2e402c4402507d84cf1e949571d15dcd
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-03-01 20:54:38 -0500

    Implemented configuration for security

--- src/main/java/com/tripgenie/config/SecurityConfig.java
+++ src/main/java/com/tripgenie/config/SecurityConfig.java
@@ -1,11 +1,76 @@
 package com.tripgenie.config;
 
+import com.tripgenie.security.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter authenticationFilter;
+    private final AccessDeniedHandler accessDeniedHandler;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
 
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationFilter authenticationFilter,
+                          AccessDeniedHandler accessDeniedHandler,
+                          AuthenticationEntryPoint authenticationEntryPoint) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationFilter = authenticationFilter;
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/api/users/**").permitAll();
+                    registry.anyRequest().authenticated();
+                })
+                .userDetailsService(userDetailsService)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exception -> {
+                    exception.accessDeniedHandler(accessDeniedHandler) // For access denied failure
+                            .authenticationEntryPoint(authenticationEntryPoint); // For other failures
+                })
+                .logout(l -> {
+                    l.logoutUrl("/logout")
+                            .logoutSuccessHandler(
+                                    (request, response, authentication) -> SecurityContextHolder.clearContext()
+                            );
+
+                });
+
+        return http.build();
+    }
 }
--- src/main/java/com/tripgenie/security/JwtAuthenticationFilter.java
+++ src/main/java/com/tripgenie/security/JwtAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package com.tripgenie.security;
 
 import com.tripgenie.service.JwtService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -25,5 +29,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        // Get auth header
+        String authHeader = request.getHeader("Authorization");
+
+        // Get token from header
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            String username = jwtService.getUsername(token);
+
+            try {
+
+                // Check if any user is not authenticated
+                if (username != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+
+                    // Get user details from username
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+                    // If userdetails aren't null and token isn't expired
+                    if (userDetails != null && !jwtService.isTokenExpired(token)) {
+                        UsernamePasswordAuthenticationToken authenticationToken =
+                                new UsernamePasswordAuthenticationToken(userDetails,
+                                        userDetails.getPassword(),
+                                        userDetails.getAuthorities()
+                                );
+
+                        // Set authentication
+                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                    }
+
+                }
+
+                // Forward the request
+                filterChain.doFilter(request, response);
+
+            } catch (ExpiredJwtException expiredJwtException) {
+                // Forward the request to your controller for handling the expired token
+                request.getRequestDispatcher("/api/users/expired-token").forward(request, response);
+            }
+        }
     }
 }
